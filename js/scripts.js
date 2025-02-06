5ce8d3f156b927e34e25268c975d2fd0939efc46
Milakrei55 <mischakom@gmail.com>
2025-02-06 10:29:56 +0100

    Changes to css, js and index as per brief

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function newItem() {
+    let inputValue = $('#input').val();
+    let li = $(`<li>${inputValue}</li>`);
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        let list = $('#list');
+        list.append(li);
+    }
+    function crossOut() {
+        li.toggleClass("strike");
+    }
+    li.on('dblclick', crossOut);
+
+    let crossOutButton = $(`<crossoutbutton>X</crossoutbutton>`)
+    li.append(crossOutButton);
+
+    crossOutButton.on('click', deleteListItem);
+    function deleteListItem() {
+        li.remove();
+    }
+    $('#list').sortable();
+
+}
